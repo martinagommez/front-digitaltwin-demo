fff49a340326a96ddf27735a2b408b5acf3fabc8
rodrigo.antunes00 <rodrigo.antunes00@outlook.com>
2025-03-21 11:42:33 +0000

    added alteration to vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["newui-aif-frontend.azurewebsites.net"]
+    allowedHosts: ["semantic-kernel-agents-orchestrator.azurewebsites.net"]
   }
 })
 
