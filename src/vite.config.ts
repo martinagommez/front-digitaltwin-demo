f1cd5115c9bbb365aeff1dd87f50e2ea295aa2a7
Cláudio Pascoal <claudio.pascoal@avanade.com>
2025-04-01 15:53:32 +0000

    Updated vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["newui-aif-frontend.azurewebsites.net"]
+    allowedHosts: ["agent-builder-app-front-2.azurewebsites.net"]
   }
 })
 
