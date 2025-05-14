0ff661fbedb99634b6d0ab73b88c1aaf87c5bf6d
Cláudio Pascoal <claudio.pascoal@avanade.com>
2025-05-14 10:19:04 +0000

    Added vite.config.ts

---
+++ src/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        allowedHosts: ["digitwins-agentic-app-front-3.azurewebsites.net"]
+    }
+})
+
+// 5173 → Default Vite dev server (local development only).
+// 3000 → Common port for local development but not required for Azure.
+// 80 or 8080 → Best choice for Azure Web App (production).
